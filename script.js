a20499003ba4fb8752cac390ac106d21cf3c85f5
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 00:08:22 -0600

    fixing save error

--- script.js
+++ script.js
@@ -6,8 +6,8 @@
 //add current date to page (currentDay)
 
 $(document).ready(function() {
-    $(".saveBtn").on("click", function () {
-        var value = $(this).siblings(".textInput").val();
+    $(".saveBtn").on("click", function(){
+        var value = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
  });
@@ -43,15 +43,15 @@ getHour();
 
 var interval = setInterval(getHour, 15000);
 
-$("#hour-9 .textInput").val(localStorage.getItem("hour-9"));
-$("#hour-10 .textInput").val(localStorage.getItem("hour-10"));
-$("#hour-11 .textInput").val(localStorage.getItem("hour-11"));
-$("#hour-12 .textInput").val(localStorage.getItem("hour-12"));
-$("#hour-13 .textInput").val(localStorage.getItem("hour-13"));
-$("#hour-14 .textInput").val(localStorage.getItem("hour-14"));
-$("#hour-15 .textInput").val(localStorage.getItem("hour-15"));
-$("#hour-16 .textInput").val(localStorage.getItem("hour-16"));
-$("#hour-17 .textInput").val(localStorage.getItem("hour-17"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
